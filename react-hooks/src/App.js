2ee2ecfcea3945fbf922c7f50e46cb1d44e9f9cb
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-21 11:00:35 +0700

    Search

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,40 +2,43 @@ import React, { Component } from "react";
 
 import { useState, useEffect } from "react";
 
-
-
-
-
 const App = () => {
-
   //state
 
-  const [news,setNews] = useState([])
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("react");
 
   //fetch news
 
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
-    .then(result => result.json())
-    .then(data => setNews(data.hits))
-    .catch(error => console.log(error) )
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+      .then((result) => result.json())
+      .then((data) => setNews(data.hits))
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    fetchNews()
-  })
-  
+    fetchNews();
+  }, [searchQuery]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
 
   return (
     <>
-    <div>
-      <h2>News</h2>
-     {news.map((blog,index) => (
-       <p key={index}>{blog.title}</p>
-     ))}
-    </div>
+      <div>
+        <h2>News</h2>
+        <form>
+          <input type="text" value={searchQuery} onChange={handleChange} />
+          <button>Search</button>
+        </form>
+        {news.map((blog, index) => (
+          <p key={index}>{blog.title}</p>
+        ))}
+      </div>
     </>
-  ) ;
+  );
 };
 
 export default App;
