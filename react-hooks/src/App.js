69db6ec6ca8a3e77d1cca5848ca7827ef40507bb
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-21 11:13:31 +0700

    Loading before fetch data

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -7,13 +7,17 @@ const App = () => {
 
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState("react");
-  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
-  //fetch news
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=react"
+  );
 
+  const [loading, setLoading] = useState(false);
+  //fetch news
   const fetchNews = () => {
+    setLoading(true);
     fetch(url)
       .then((result) => result.json())
-      .then((data) => setNews(data.hits))
+      .then((data) => (setNews(data.hits), setLoading(false)))
       .catch((error) => console.log(error));
   };
 
@@ -27,13 +31,14 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
-  }
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
 
   return (
     <>
       <div>
         <h2>News</h2>
+        {loading ? <h2> Loading... </h2> : ""}
         <form onSubmit={handleSubmit}>
           <input type="text" value={searchQuery} onChange={handleChange} />
           <button>Search</button>
